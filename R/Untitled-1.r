d7cf7524067595606dcc7fa9e342b5a38a1c0cce
primojr <gersonprimo@gmail.com>
2022-08-03 17:20:32 -0300

    teste

--- R/Untitled-1.r
+++ R/Untitled-1.r
@@ -3,15 +3,15 @@ getwd()
 setwd()
 #"C:\\Users\\gerson.junior\\AppData\\Local\\Packages\\PythonSoftwareFoundation.Python.3.9_qbz5n2kfra8p0\\LocalCache\\local-packages\\Python39\\Scripts\\radian.exe"
 usethis::create_project("C:/R/Dir/vscode_r")
-usethis::use_git("C:/R/Dir/vscode_r")
+usethis::use_git()
 
-usethis::use_github("C:/R/Dir/vscode_r")
+usethis::use_github()
 
 # Bibliotecas
 library(tidyverse)
 
 # Ler base
-df <- read_csv('C:/R/Dir/data.frame/ju.csv')
+df <- read_csv('C:/R/Dir/data.frame/jus.csv')
 
 names(df)
 
